3541b722083fe9c5f714e12e0960853aa089d20a
derya <lacin>
2024-11-15 22:16:05 +0100

    some css adjustements

--- script2.js
+++ script2.js
@@ -1,24 +1,4 @@
-//Dropdown Menu
-function myFunction(dropdownNumber) {
-  var dropdown = document.getElementById("myDropdown" + dropdownNumber);
-  var dropdowns = document.getElementsByClassName("dropdown-content");
-  for (var i = 0; i < dropdowns.length; i++) {
-    if (dropdowns[i] !== dropdown) {
-      dropdowns[i].classList.remove("show");
-    }
-  }
 
-  dropdown.classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var i = 0; i < dropdowns.length; i++) {
-      dropdowns[i].classList.remove("show");
-    }
-  }
-};
 
 //POPUP MOVIE DETAIL
 
